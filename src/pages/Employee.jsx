2dee71fbd477196cf0f119d5bac7b6ecbf3ef338
markkybean <marksantos320@gmail.com>
2023-12-13 01:06:33 +0800

    added some updates

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,17 +1,33 @@
-function Employee({firstname, lastname, grade, deleteEmployee,updateEmployee, employeeID}){
-    return(
-        <div className="alert alert-light bg-dark-subtle shadow-lg">
-            {lastname}, {firstname}<span className="badge bg-dark ms-2">{grade}</span>
-            <button onClick={()=>{
-                deleteEmployee(employeeID, firstname, lastname);
-            }}
-             className="btn btn-danger btn-sm float-end">Delete</button>
-            <button onClick={()=>{
-                updateEmployee(employeeID, firstname, lastname,grade);
-            }}
-             className="btn btn-info btn-sm float-end me-2">Edit</button>
-            
-        </div>
-    )
-}
-export default Employee;
+function Employee({ firstname, lastname, deleteEmployee, updateEmployee, employeeID, email, phoneNumber, address, department }) {
+    return (
+      <div className="alert alert-light bg-dark-subtle shadow-lg">
+        {lastname}, {firstname}
+        <button
+          onClick={() => {
+            deleteEmployee(employeeID, firstname, lastname);
+          }}
+          className="btn btn-danger btn-sm float-end"
+        >
+          Delete
+        </button>
+        <button
+          onClick={() => {
+            updateEmployee(
+              employeeID,
+              firstname,
+              lastname,
+              email,
+              phoneNumber,
+              address,
+              department
+            );
+          }}
+          className="btn btn-info btn-sm float-end me-2"
+        >
+          Edit
+        </button>
+      </div>
+    );
+  }
+  export default Employee;
+  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,15 @@ import firebaseApp from "./firebaseConfig";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Home(){
-    const [employee, setEmployee] = useState({
-        firstname: '',
-        lastname: '',
-        grade: '',
-    });
+  const [employee, setEmployee] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+    department: '',
+  });
+  
 
     const [employeeList, setEmployeeList] = useState([]);
 
@@ -64,34 +68,49 @@ export default function Home(){
 
 
     // create 
-    const addEmployee = () =>{
-        // initialize cloud firestore and get a reference to the service
-        const db = getFirestore(firebaseApp);
+    const addEmployee = () => {
+      // Initialize cloud firestore and get a reference to the service
+      const db = getFirestore(firebaseApp);
+    
+      // Check for missing required fields
+      if (
+        employee.firstname === '' ||
+        employee.lastname === '' ||
+        employee.email === '' ||
+        employee.phoneNumber === '' ||
+        employee.address === '' ||
+        employee.department === ''
+        // Add other required fields as needed
+      ) {
+        alert("Missing Fields");
+      } else {
+        // Update local state
+        setEmployeeList([...employeeList, employee]);
+    
+        // Add employee to Firestore
+        addDoc(collection(db, 'employees'), employee)
+          .then(() => {
+            console.log("Employee added to Firestore");
+          })
+          .catch((error) => {
+            console.error("Error adding employee to Firestore: ", error);
+            // Handle error if addition fails
+          });
+    
+        // Clear form fields
+        setEmployee({
+          firstname: '',
+          lastname: '',
+          email: '',
+          phoneNumber: '',
+          address: '',
+          department: '',
+          // Add other fields as needed
+        });
 
-        if(employee.firstname=== '' || employee.lastname==='' || employee.grade===''){
-            alert("Missing Fields")
-
-        }else{
-            setEmployeeList(
-                employeelist => [
-                    ...employeeList, employee
-                ]
-            );
-
-            addDoc(collection(db, 'employees'), employee);
-                
-            setEmployee({
-                // remove yung nakalagay sa form na sulat
-                firstname: '',
-                lastname: '',
-                grade: '',
-            });
-
-            
-            // localStorage.setItem('employeeList', JSON.stringify(employeeList));  
-        }
-        
-    }
+      }
+    };
+    
 
     // delete function
 
@@ -109,123 +128,204 @@ export default function Home(){
 
     // update employee/ edit
 
-    const updateEmployee = (employeeID, firstname, lastname, grade) => {
-        setEditToggle(true);
-        setEmployee({
-            employeeID: employeeID,
-            firstname: firstname,
-            lastname: lastname,
-            grade: grade
-        });
-    }
+    const updateEmployee = (employeeID, firstname, lastname, email, phoneNumber, address, department) => {
+      setEditToggle(true);
+      setEmployee({
+        employeeID: employeeID,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phoneNumber: phoneNumber,
+        address: address,
+        department: department,
+        // Add other fields as needed
+      });
+    };
+    
 
-    const handleEmployeeUpdate = () => {
-        const db = getFirestore(firebaseApp);
+  const handleEmployeeUpdate = () => {
+    const db = getFirestore(firebaseApp);
 
-        const employeeRef = doc(db, "employees", employee.employeeID);
+    const employeeRef = doc(db, "employees", employee.employeeID);
 
-        updateDoc(employeeRef, {
-            firstname: employee.firstname,
-            lastname: employee.lastname,
-            grade: employee.grade
-        });
-
-        setEditToggle(false);
-        setEmployee({
-            // remove yung nakalagay sa form na sulat
-            firstname: '',
-            lastname: '',
-            grade: '',
-        });
-    }
+    updateDoc(employeeRef, {
+      firstname: employee.firstname,
+      lastname: employee.lastname,
+      email: employee.email,
+      phoneNumber: employee.phoneNumber,
+      address: employee.address,
+      department: employee.department,
+      // Add other fields as needed
+    }).then(() => {
+      setEditToggle(false);
+      setEmployee({
+        // Clear all fields after update
+        firstname: '',
+        lastname: '',
+        email: '',
+        phoneNumber: '',
+        address: '',
+        department: '',
+      });
+    }).catch((error) => {
+      // Handle error if update fails
+      console.error("Error updating document: ", error);
+    });
+  };
+    
 
     if(authenticated){
       return (
         <section className="container">
-          <h1 className="fw-bold">🎓Employee Record🎓</h1>
+          <h1 className="fw-bold text-center">🎓Employee Record🎓</h1>
           <h3 className="fw-bold">Hello, {userProperties.displayName} </h3>
           <p>This is the list of employee records.</p>
-          <div className="mb-5 p-5 border bg-dark-subtle shadow-lg">
-            <div className="row">
-              <div className="col-md-5 col-lg-5 col-sm-12">
-                <label htmlFor="firstname">First name:</label>
-                <input
-                  onChange={(e) =>
-                    setEmployee({
-                      ...employee,
-                      firstname: e.target.value,
-                    })
-                  }
-                  value={employee.firstname}
-                  type="text"
-                  className="form-control"
-                  id="firstname"
-                />
-              </div>
-              <div className="col-md-4 col-lg-5 col-sm-12">
-                <label htmlFor="lastname">Last name:</label>
-                <input
-                  onChange={(e) =>
-                    setEmployee({
-                      ...employee,
-                      lastname: e.target.value,
-                    })
-                  }
-                  value={employee.lastname}
-                  type="text"
-                  className="form-control"
-                  id="lastname"
-                />
-              </div>
-              <div className="col-md-2 col-lg-1 col-sm-2 pe-1">
-                <label htmlFor="grade">Grade:</label>
-                <input
-                  onChange={(e) =>
-                    setEmployee({
-                      ...employee,
-                      grade: e.target.value,
-                    })
-                  }
-                  value={employee.grade}
-                  type="number"
-                  className="form-control"
-                  id="grade"
-                />
-              </div>
-  
-              {editToggle ? (
-                // kung true magiging update, pag cinlick yung edit
-                <div className="col-md-2 col-lg-2 col-sm-12">
-                  <button
-                    onClick={() => {
-                      handleEmployeeUpdate();
-                    }}
-                    className="btn btn-success mt-4"
-                  >
-                    Update
-                  </button>
-                </div>
-              ) : (
-                <div className="col-md-2 col-lg-2 col-sm-12">
-                  <button
-                    onClick={() => {
-                      addEmployee();
-                    }}
-                    className="btn btn-dark mt-4"
-                  >
-                    Add➕
-                  </button>
-                </div>
-              )}
-  
-              <div className="alert alert-light mt-3">
+          
+            {/* Button trigger modal */}
+            <button type="button" className="btn btn-dark mb-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Add employee
+            </button>
+
+            {/* Modal */}
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog modal-xl">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">Add Employee Information</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body">
+                  <div className="mb-5 p-5 border bg-dark-subtle shadow-lg">
+                  <div className="row">
+                    <div className="col-md-5 col-lg-5 col-sm-12">
+                      <label htmlFor="firstname">First name:</label>
+                      <input
+                        onChange={(e) =>
+                          setEmployee({
+                            ...employee,
+                            firstname: e.target.value,
+                          })
+                        }
+                        value={employee.firstname}
+                        type="text"
+                        className="form-control"
+                        id="firstname"
+                      />
+                    </div>
+                    <div className="col-md-4 col-lg-5 col-sm-12">
+                      <label htmlFor="lastname">Last name:</label>
+                      <input
+                        onChange={(e) =>
+                          setEmployee({
+                            ...employee,
+                            lastname: e.target.value,
+                          })
+                        }
+                        value={employee.lastname}
+                        type="text"
+                        className="form-control"
+                        id="lastname"
+                      />
+                    </div>
+                    <div className="col-md-4 col-lg-5 col-sm-12">
+                      <label htmlFor="email">Email:</label>
+                      <input
+                        onChange={(e) =>
+                          setEmployee({
+                            ...employee,
+                            email: e.target.value,
+                          })
+                        }
+                        value={employee.email}
+                        type="text"
+                        className="form-control"
+                        id="email"
+                      />
+                    </div>
+                    <div className="col-md-4 col-lg-5 col-sm-12">
+                      <label htmlFor="phoneNumber">Phone Number:</label>
+                      <input
+                        onChange={(e) =>
+                          setEmployee({
+                            ...employee,
+                            phoneNumber: e.target.value,
+                          })
+                        }
+                        value={employee.phoneNumber}
+                        type="text"
+                        className="form-control"
+                        id="phoneNumber"
+                      />
+                    </div>
+                    <div className="col-md-4 col-lg-5 col-sm-12">
+                      <label htmlFor="address">Address:</label>
+                      <input
+                        onChange={(e) =>
+                          setEmployee({
+                            ...employee,
+                            address: e.target.value,
+                          })
+                        }
+                        value={employee.address}
+                        type="text"
+                        className="form-control"
+                        id="address"
+                      />
+                    </div>
+                    <div className="col-md-4 col-lg-5 col-sm-12">
+                      <label htmlFor="department">Department/Team:</label>
+                      <input
+                        onChange={(e) =>
+                          setEmployee({
+                            ...employee,
+                            department: e.target.value,
+                          })
+                        }
+                        value={employee.department}
+                        type="text"
+                        className="form-control"
+                        id="department"
+                      />
+                    </div>
+
+
+
+                    {/* Display current employee details */}
+                    {/* <div className="alert alert-light mt-3">
                 <h3 className="fw-bold">
                   {employee.firstname} {employee.lastname}{" "}
-                  <span className="badge bg-dark">{employee.grade}</span>{" "}
                 </h3>
+              </div> */}
+                  </div>
+                  </div>
+                  
+                  </div>
+                  <div className="modal-footer">
+                    {/* Buttons */}
+                    <div className="col-md-2 col-lg-2 col-sm-12">
+                      {editToggle ? (
+                        <button
+                          onClick={handleEmployeeUpdate}
+                          className="btn btn-success mt-4"
+                        >
+                          Update
+                        </button>
+                      ) : (
+                        <button
+                          onClick={addEmployee}
+                          className="btn btn-dark mt-4"
+                        >
+                          Add➕
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+            
+            
+
   
   
           {/* kapag 0 lalabas no employee */}
@@ -245,14 +345,18 @@ export default function Home(){
             <>
               {employeeList.map((employeeRecord) => (
                 <Employee
-                  key={employeeRecord.employee_id}
-                  firstname={employeeRecord.firstname}
-                  lastname={employeeRecord.lastname}
-                  grade={employeeRecord.grade}
-                  deleteEmployee={deleteEmployee}
-                  updateEmployee={updateEmployee}
-                  employeeID={employeeRecord.employee_id}
-                />
+                key={employeeRecord.employee_id}
+                firstname={employeeRecord.firstname}
+                lastname={employeeRecord.lastname}
+                email={employeeRecord.email}
+                phoneNumber={employeeRecord.phoneNumber}
+                address={employeeRecord.address}
+                department={employeeRecord.department}
+                deleteEmployee={deleteEmployee}
+                updateEmployee={updateEmployee}
+                employeeID={employeeRecord.employee_id}
+              />
+              
               ))}
             </>
           )}
